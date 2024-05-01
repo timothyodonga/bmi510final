ffe9efd2800cd6c57d14218771b7421fc1eb362c
Timothy Odonga <timothy.odonga@gmail.com>
2024-05-01 16:57:29 -0400

    Working code checkpoint

--- R/functions.R
+++ R/functions.R
@@ -255,7 +255,7 @@ minimumN <- function(x1, x2 = NULL) {
   if (is.null(x2)) {
     # One-sample t-test
     result <- pwr::pwr.t.test(d = mean(x1), sig.level = 0.05, power = 0.8, alternative = "two.sided")
-    return((result$n))
+    return(ceiling(result$n))
   } else {
     # Two-sample t-test
     result <- pwr::pwr.t2n.test(n1 = length(x1), n2 = length(x2), sig.level = 0.05, power = 0.8, alternative = "two.sided")
